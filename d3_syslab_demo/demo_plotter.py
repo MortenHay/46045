21274c0f09f4cd76b8c4b11c1b0914f833e5bdcc
MortenHay <haymorten@gmail.com>
2024-06-10 13:32:03 +0200

    combine

--- d3_syslab_demo/demo_plotter.py
+++ d3_syslab_demo/demo_plotter.py
@@ -1,3 +1,4 @@
+# %%
 import pandas as pd
 import json
 import matplotlib.pyplot as plt
@@ -5,10 +6,16 @@ import os
 from datetime import timedelta
 import syslab
 
-## Read the measurements data file ##
-DATA_MEAS_DIR = 'data\measurements'
-# Always plot latest datafile - replace [-1] with another index if you want to plot a specific file.
-MEAS_LOG_FILE = sorted(os.listdir(DATA_MEAS_DIR))[-1]
+try:
+    ## Read the measurements data file ##
+    DATA_MEAS_DIR = "data\measurements"
+    # Always plot latest datafile - replace [-1] with another index if you want to plot a specific file.
+    MEAS_LOG_FILE = sorted(os.listdir(DATA_MEAS_DIR))[-1]
+except FileNotFoundError:
+    ## Read the measurements data file ##
+    DATA_MEAS_DIR = "data/measurements"
+    # Always plot latest datafile - replace [-1] with another index if you want to plot a specific file.
+    MEAS_LOG_FILE = sorted(os.listdir(DATA_MEAS_DIR))[-1]
 
 # Store each dictionary of the measurements json in a list
 with open(os.path.join(DATA_MEAS_DIR, MEAS_LOG_FILE)) as f:
@@ -20,9 +27,14 @@ use_setpoint_log = False
 
 ## Read the setpoints data file ##
 if use_setpoint_log:
-    DATA_SP_DIR = 'data\setpoints'
-    # Always plot latest datafile
-    SP_LOG_FILE = sorted(os.listdir(DATA_SP_DIR))[-1]
+    try:
+        DATA_SP_DIR = "data\setpoints"
+        # Always plot latest datafile
+        SP_LOG_FILE = sorted(os.listdir(DATA_SP_DIR))[-1]
+    except FileNotFoundError:
+        DATA_SP_DIR = "data/setpoints"
+        # Always plot latest datafile
+        SP_LOG_FILE = sorted(os.listdir(DATA_SP_DIR))[-1]
 
     # Store each dictionary of the setpoints json in a list
     with open(os.path.join(DATA_SP_DIR, SP_LOG_FILE)) as f:
@@ -37,7 +49,7 @@ else:
 
 # Construct a dataframe and pivot it to obtain a dataframe with a column per unit, and a row per timestamp.
 df = pd.DataFrame.from_records(data)
-df_pivot = df.pivot_table(values='value', columns='unit', index='time')
+df_pivot = df.pivot_table(values="value", columns="unit", index="time")
 
 
 # Plot the data. Note, that the data will mostly not be plotted with lines.
@@ -45,35 +57,49 @@ plt.ion()  # Turn interactive mode on
 plt.figure()
 ax1 = plt.subplot(211)  # Make two separate figures
 ax2 = plt.subplot(212)
-df_pivot[[c for c in df_pivot.columns if "_p" in c]].plot(marker='.', ax=ax1, linewidth=3)
-df_pivot[[c for c in df_pivot.columns if "_q" in c]].plot(marker='.', ax=ax2, linewidth=3)
+df_pivot[[c for c in df_pivot.columns if "_p" in c]].plot(
+    marker=".", ax=ax1, linewidth=3
+)
+df_pivot[[c for c in df_pivot.columns if "_q" in c]].plot(
+    marker=".", ax=ax2, linewidth=3
+)
 plt.show(block=True)
 
-#%%
+# %%
 ## TODO Q1: Your code here
 plt.figure(figsize=(10, 6))
-plt.plot(df_pivot.iloc[:, ::2],df_pivot.iloc[:, 1::2], marker='.', linewidth=3)
+plt.plot(df_pivot.iloc[:, ::2], df_pivot.iloc[:, 1::2], marker=".", linewidth=3)
 plt.legend(df_pivot.columns[::2])
-plt.xlabel('P')
-plt.ylabel('Q')
-plt.title('P vs Q')
+plt.xlabel("P")
+plt.ylabel("Q")
+plt.title("P vs Q")
 plt.show()
 
-#%%
+# %%
+ncol = 3
+nrow = 3
+fig, axs = plt.subplots(nrows=nrow, ncols=ncol)
+units = [c[:-2] for c in df_pivot.columns if "_p" in c]
+for i in range(len(units)):
+    ax = axs.reshape(-1)[i]
+    ax.plot(df_pivot[units[i] + "_q"], df_pivot[units[i] + "_p"], label=units[i])
+    ax.title.set_text(units[i])
+
+fig.tight_layout()
+
+# %%
 ## TODO Q2:
 # Convert time column (index) of df_pivot to datetime
 # TODO Your code here
 # Hint1: You can use pandas to_numeric() to prepare the index for pandas to_datetime function
 # Hint2: Remember to define the unit within pandas to_datetime function
 
-df_pivot.index = pd.to_datetime(df_pivot.index, unit='s')
+df_pivot.index = pd.to_datetime(df_pivot.index, unit="s")
 
-#%%
 # Resample the data
 # TODO Your code here
-df_resampled = df_pivot.resample('s').mean()
+df_resampled = df_pivot.resample("s").mean()
 
-#%%
 # Interpolate the measurements
 # TODO Your code here
 # Hint: For part two of the exercise ("collecting fresh data") the nan rows after a setpoint
@@ -81,22 +107,16 @@ df_resampled = df_pivot.resample('s').mean()
 # You can use the df.fillna(method="ffill") function for that purpose. However, the measurements should still be interpolated!
 df_resampled = df_resampled.interpolate()
 
-#%%
 # Plot the resampled data
 # TODO Your code here
-df_resampled.plot(marker='.', linewidth=3)
-
-#%%
+df_resampled.plot(marker=".", linewidth=3)
 
 
-#%%
+# %%
 ## TODO Q3: Your code here
 
 
-
-
 ## TODO Q4: Your code here
-
 
 
 ## Part two: "Collecting fresh data"
@@ -113,12 +133,11 @@ if use_setpoint_log:
     # Loop over all steps and extract T_1, T_2 and the step size
     results = {}
 
-    for idx in range(0, len(sp_data)-1):
+    for idx in range(0, len(sp_data) - 1):
         label = f"Step_{sp_data[idx]['value']}kW"
 
         # Extract T_1 and T_2 from the setpoint JSON
         # TODO your code here
-
 
         # Change timestamp format
         T_1 = pd.to_datetime(pd.to_numeric(T_1), unit="s").round("0.1S")
@@ -126,7 +145,6 @@ if use_setpoint_log:
 
         # To ensure we are not considering values of the next load step
         T_2 = T_2 - timedelta(seconds=0.2)
-
 
         # define measured output y and target setpoint r
         # TODO your code here
@@ -142,10 +160,8 @@ if use_setpoint_log:
             # TODO your code here
         }
 
-    pd.DataFrame.from_dict(results).plot(kind='bar')
+    pd.DataFrame.from_dict(results).plot(kind="bar")
     plt.title("Metrics")
     plt.tight_layout()
-    plt.savefig('data/test_metrics'+MEAS_LOG_FILE[-10:]+'.png')
+    plt.savefig("data/test_metrics" + MEAS_LOG_FILE[-10:] + ".png")
     plt.show(block=True)
-
-
